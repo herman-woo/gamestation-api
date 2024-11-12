e4703e86c01037766980adf5408aa0e855a8773d
Herman Woo <hwoo02250@gmail.com>
2024-11-11 22:04:54 -0500

    updating media

--- gamestation_api/settings.py
+++ gamestation_api/settings.py
@@ -27,8 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 # settings.py
-MEDIA_URL = '/media/'
-
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Application definition
 
@@ -127,5 +127,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
