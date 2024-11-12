5b194e34eaf1cbf4b51c6622e6d0b860c68997d8
Herman Woo <hwoo02250@gmail.com>
2024-11-11 20:45:08 -0500

    New application

--- games/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'Games'
-
-urlpatterns = [
-    path('', views.index, name="index"),
-    path('<int:id>/', views.get_book, name="get book")
-]
